1deb7aaa9000d31f8615d2a676e271668d47a2c1
abhinandanchivate <abhinadanchivate2000@gmail.com>
2025-06-11 05:18:17 +0530

    L2 is updated for array

--- L2.js
+++ L2.js
@@ -7,12 +7,16 @@ const walkStrategy = (bird) => {
 const flyStrategy = (bird) => {
   console.log(`${bird.name} is flying.`);
 };
-const sparrow = { name: "Sparrow" };
-const penguin = { name: "Penguin" };
-const ostrich = { name: "Ostrich" };
-const eagle = { name: "Eagle" };
+const sparrow = { name: "Sparrow", move: "fly" }; // move
+const penguin = { name: "Penguin", move: "walk" };
+const ostrich = { name: "Ostrich", move: "walk" };
+const eagle = { name: "Eagle", move: "fly" };
 
-moveBird(flyStrategy, sparrow); // Sparrow is flying.
-moveBird(walkStrategy, penguin); // Penguin is walking.
-moveBird(walkStrategy, ostrich); // Ostrich is walking.
-moveBird(flyStrategy, eagle); // Eagle is flying.
+const birds = [sparrow, penguin, ostrich, eagle];
+birds.forEach((bird) => {
+  if (bird.move === "fly") {
+    moveBird(flyStrategy, bird);
+  } else if (bird.move === "walk") {
+    moveBird(walkStrategy, bird);
+  }
+});
